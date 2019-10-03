54d14c7caa94bed795eae2e47226cdcbcfe86bd8
Isaac <isaac.chang5@gmail.com>
2019-10-03 00:09:39 -0700

    Updates

---
+++ precache-manifest.2db4af2cbd7dc86d1c4162a1d7de1fed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3af41ca5dcccba0cf05497abcb2fd783",
+    "url": "/React-Gallery-App/index.html"
+  },
+  {
+    "revision": "9e782ec1af3fbb310f20",
+    "url": "/React-Gallery-App/static/css/main.c2d25503.chunk.css"
+  },
+  {
+    "revision": "af7201416f0bd9b80ae8",
+    "url": "/React-Gallery-App/static/js/2.478ec9e0.chunk.js"
+  },
+  {
+    "revision": "9e782ec1af3fbb310f20",
+    "url": "/React-Gallery-App/static/js/main.2bf42b3d.chunk.js"
+  },
+  {
+    "revision": "b7ef7c30157e4114a2b3",
+    "url": "/React-Gallery-App/static/js/runtime-main.adce08df.js"
+  }
+]);
